cec00aec91b1b22299d0b9ca97f2e729fffeeb01
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-03-26 11:43:06 +0530

    init

--- HJ/lambda.js
+++ HJ/lambda.js
@@ -1,12 +1,23 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
-	let arr=[];
-	for(i=0;i<100;i++){
-		if(i%2==0){
+	let arr = [];
+	for (i = 0; i < 100; i++) {
+		if (i % 2 == 0) {
 			arr.push(i);
 		}
 	}
-
-	callback(null,arr);
+	s3.putObject({
+		"Body": JSON.stringify(arr),
+		"Bucket": "com.sigma.upload.test",
+		"Key": "arr"
+	})
+		.promise()
+		.then(data => {
+			callback(null, arr);
+		})
+		.catch(err => {
+			callback(arr, null);
+		});
 }
 
